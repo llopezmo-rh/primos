acf44ca6c41a5e7561103b9c067258c51a34f747
root <root@rocky1.llopezmo-thinkpadp1gen3.rmtes.csb>
2025-11-25 12:55:01 +0100

    Function find_divisor taken out from primos.c in order to be shared with other implementations

---
+++ find_divisor.h
@@ -0,0 +1,8 @@
+#ifndef FIND_DIVISOR_H
+#define FIND_DIVISOR_H
+
+#include <gmp.h>
+
+int find_divisor(mpz_t divisor, const mpz_t n);
+
+#endif
--- primos.c
+++ primos.c
@@ -12,6 +12,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
+#include "find_divisor.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -48,42 +49,6 @@ void usage()
 		"-n <opt_n_output>: limit amount of numbers to calculate. With -p only prime numbers will be count.\n"
 		"-p: show only prime numbers.\n"
 		);
-	}
-
-// find_divisor stores on "divisor" the first divisor of "n" found different to 1.
-// If none is found, it will set "divisor" to 1.
-// It returns 0 (false) if no divisor different to 1 has been found. Otherwise 1.
-// "divisor" must have been initialized before calling the function
-int find_divisor(mpz_t divisor, const mpz_t n)
-	{
-	mpz_t i, last_divisor;
-	if (mpz_cmp_ui(n, 2) == 0)
-		{       
-		mpz_set_ui(divisor, 1);
-		return 0;
-		}
-	if (mpz_even_p(n))
-		{
-		mpz_set_ui(divisor, 2);
-		return 1;
-		}     
-	mpz_set_ui(divisor, 1);
-	mpz_init_set_ui(i, 3);
-	mpz_init(last_divisor);
-	mpz_sqrt(last_divisor, n);
-	while (mpz_cmp(i, last_divisor) <= 0)
-		{
-		if (mpz_divisible_p(n, i))
-			{
-			mpz_set(divisor, i);
-			break;
-			}
-		else
-			mpz_add_ui(i, i, 2);
-		}
-	mpz_clear(i);
-	mpz_clear(last_divisor);
-	return mpz_cmp_ui(divisor, 1) > 0;
 	}
 
 int main(int argc, char *argv[])
